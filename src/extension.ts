fa3592aeb543203a3872465f17500e87421a0520
Yura Myhaylyak <ghost86421@gmail.com>
2020-02-22 11:33:12 +0200

    Added .editorconfig; Added simple `onDidSaveTextDocument` event handler

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "c4-model-visualizer-vscode" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "c4-model-visualizer-vscode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('c4Visualizer.start', () => {
-		// The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  vscode.workspace.onDidSaveTextDocument((e) => {
+      vscode.window.showInformationMessage(e.getText());
+  });
 
-		// Create and show a new webview
-		const panel = vscode.window.createWebviewPanel(
-			'c4Visualizer',
-			'C4 Visualizer',
-			vscode.ViewColumn.Two,
-			{},
-		);
+  let disposable = vscode.commands.registerCommand('c4Visualizer.start', () => {
+    // The code you place here will be executed every time your command is executed
 
-		panel.webview.html = getWebviewContent();
-	});
+    // Create and show a new webview
+    const editor = vscode.window.activeTextEditor;
+    const panel = vscode.window.createWebviewPanel(
+      'c4Visualizer',
+      'C4 Visualizer',
+      vscode.ViewColumn.Two,
+      {},
+    );
 
-	context.subscriptions.push(disposable);
+    panel.webview.html = getWebviewContent();
+
+    if (!editor) {
+      vscode.window.showInformationMessage('Editor does not exist');
+      return;
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
 function getWebviewContent() {
-	return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
